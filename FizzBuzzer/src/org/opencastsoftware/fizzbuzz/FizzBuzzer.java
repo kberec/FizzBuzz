0ccd3d9ae4b250946c2cf96b379962c551de875f
kberec <berec.kornel@gmail.com>
2017-11-09 12:43:52 +0000

    Extended solution to include statistics.

--- FizzBuzzer/src/org/opencastsoftware/fizzbuzz/FizzBuzzer.java
+++ FizzBuzzer/src/org/opencastsoftware/fizzbuzz/FizzBuzzer.java
@@ -1,10 +1,15 @@
 package org.opencastsoftware.fizzbuzz;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * Class to print out various keywords for contiguous range of numbers
  * @author Kornel
  *
  */
 public class FizzBuzzer {
+	private Map<String,Integer> statistics = new LinkedHashMap<>();
 	
 	/**
 	 * Print out keywords based on range of numbers 
@@ -12,19 +17,58 @@ public class FizzBuzzer {
 	 * @param toRange the ending number of the range
 	 */
 	public void print(int fromRange, int toRange) {
+		initializeStatistics();
+		
 		for (int i = fromRange; i <= toRange; i++) {
-			if (String.valueOf(i).contains("3")) 
-				System.out.print("lucky");				
+			if (String.valueOf(i).contains("3"))
+				handleKeyword("lucky", 0);
 			else if (i % 3 == 0 && i % 5 == 0)
-				System.out.print("fizzbuzz");
+				handleKeyword("fizzbuzz", 0);
 			else if (i % 3 == 0)
-				System.out.print("fizz");
+				handleKeyword("fizz", 0);
 			else if (i % 5 == 0)
-				System.out.print("buzz");
+				handleKeyword("buzz", 0);
 			else
-				System.out.print(i);
+				handleKeyword("number", i);
 			
 			System.out.print(" ");				
 		}
+		
+		printStatistics();
 	}
+
+	/**
+	 * Initialize statistics
+	 */
+	private void initializeStatistics() {
+		String[] keywords = "fizz,buzz,fizzbuzz,lucky,number".split(",");
+		for(String keyword: keywords)
+		{
+			statistics.put(keyword, 0);			
+		}
+	}
+
+	/**
+	 * Increase counters in statistics and print out the keyword 
+	 * @param keyword
+	 */
+	private void handleKeyword(String keyword, int value) {
+		statistics.put(keyword, statistics.get(keyword) + 1);
+		
+		if (value != 0)
+			System.out.print(value);
+		else
+			System.out.print(keyword);
+	}
+	
+	/**
+	 * Print out statistics
+	 */
+	private void printStatistics() {
+		System.out.print(System.getProperty("line.separator"));
+		for (Map.Entry<String, Integer> item : statistics.entrySet()) {
+			System.out.println(item.getKey() + ": " + item.getValue());
+		}
+	}
+
 }
--- FizzBuzzer/test/org/opencastsoftware/fizzbuzz/FizzBuzzerTest.java
+++ FizzBuzzer/test/org/opencastsoftware/fizzbuzz/FizzBuzzerTest.java
@@ -27,7 +27,13 @@ public class FizzBuzzerTest {
 	@Test
 	public void test() {
 		fizzBuzzer.print(1, 20);
-		String expectedPrintout = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz ";
+		String expectedPrintout = "1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz " + System.getProperty("line.separator") + 
+				"fizz: 4" + System.getProperty("line.separator") +
+				"buzz: 3" + System.getProperty("line.separator") + 
+				"fizzbuzz: 1" + System.getProperty("line.separator") + 
+				"lucky: 2" + System.getProperty("line.separator") + 
+				"number: 10" + System.getProperty("line.separator");
+		;
 		Assert.assertEquals(expectedPrintout, outContent.toString());		
 	}
 
